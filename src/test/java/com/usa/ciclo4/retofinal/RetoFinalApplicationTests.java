2f70948574e29bd2cd4cea522732759241c11f87
Yenny <yenny.waitoto.mt@correo.usa.edu.co>
2021-12-22 11:06:43 -0500

    Iniciando el repositorio

---
+++ src/test/java/com/usa/ciclo4/retofinal/RetoFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.usa.ciclo4.retofinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetoFinalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
